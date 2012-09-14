494f493bffe8d4cd09d8327b1574d34be946b9d8
Sam Smoot <ssmoot@gmail.com>
2012-09-14 14:19:22 -0500

    Updated with results for a much larger (and a little more complex, including Arrays and such) document.

--- jackson_benchmark.rb
+++ jackson_benchmark.rb
@@ -71,3 +71,11 @@ Taking 10 measurements of at least 1.0s
 JSON            7.997 µs/i ± 0.814 (10.2%) <=> 125054 ips
 Jackson         4.180 µs/i ± 0.594 (14.2%) <=> 239222 ips
 Jackson Wrapper 4.712 µs/i ± 1.239 (26.3%) <=> 212228 ips
+
+#########################################################
+## perfer results with a much larger (26K) document:
+
+Taking 10 measurements of at least 1.0s
+JSON            1.041 ms/i ± 0.088 ( 8.4%) <=> 960 ips
+Jackson         356.8 µs/i ± 27.36 ( 7.7%) <=> 2803 ips
+Jackson Wrapper 350.9 µs/i ± 2.912 ( 0.8%) <=> 2850 ips
