73e573548418979339735b93710bbace900eb6f7
Sam Smoot <ssmoot@gmail.com>
2012-10-02 15:13:33 -0500

    Added install command. closes #21.

---
+++ lib/doubleshot/commands/install.rb
@@ -0,0 +1,35 @@
+class Doubleshot::CLI::Commands::Install < Doubleshot::CLI
+
+  def self.summary
+    <<-EOS.margin
+      Install your project as a Rubygem.
+    EOS
+  end
+
+  def self.options
+    Options.new do |options|
+      options.banner = "Usage: doubleshot install"
+      options.separator ""
+      options.separator "Options"
+
+      options.test = true
+      options.on "--no-test", "Disable testing as a build prerequisite." do
+        options.test = false
+      end
+
+      options.separator ""
+      options.separator "Summary: #{summary}"
+    end
+  end
+
+  def self.start(args)
+    Doubleshot::CLI::Commands::Gem.start(args)
+
+    require "rubygems/dependency_installer"
+
+    installer = ::Gem::DependencyInstaller.new
+    installer.install Doubleshot::current.config.gemspec.file_name
+    
+    return true
+  end
+end
