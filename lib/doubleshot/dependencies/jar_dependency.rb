f4681e8f14c7fa851fc8a81b93c5936edfda5a2b
Sam Smoot <ssmoot@gmail.com>
2012-10-03 11:55:08 -0500

    Added TODO notes to JarDependency.

--- lib/doubleshot/dependencies/jar_dependency.rb
+++ lib/doubleshot/dependencies/jar_dependency.rb
@@ -12,6 +12,17 @@ class Doubleshot
           raise ArgumentError.new(%q<+name+ must be a String with the format "groupId:artifactId:packageType:version" (http://buildr.apache.org/quick_start.html#dependencies)>)
         end
       end
+
+      def path
+        # TODO: THINGS
+        # Give us back the path to the (presumably) JAR in the
+        # local Maven repository (usually $HOME/.m2/).
+        #
+        # OR?
+        # 
+        # If the JAR has been vendored-in, then return that path.
+        # All depends on the lock-file I guess?
+      end
     end
   end
 end
