546b3549d3625f8b217195a7d28970af3cb7e13c
Sam Smoot <ssmoot@gmail.com>
2012-10-04 12:16:30 -0500

    Made Classpath enumerable, added #size, and tested #add behavior.

--- lib/doubleshot/compiler/classpath.rb
+++ lib/doubleshot/compiler/classpath.rb
@@ -1,11 +1,36 @@
+require "set"
+
 class Doubleshot
   class Compiler
     class Classpath
 
+      include Enumerable
+
+      def initialize
+        @set = Set.new
+      end
+
       def add(path)
         path = Pathname(path.to_s)
-        path.realpath
+        
+        if path.directory?
+          @set << path
+        elsif path.file?
+          @set << path.dirname
+        else
+          raise Errno::ENOENT.new(path.expand_path.to_s)
+        end
+
         self
+      end
+
+      def size
+        @set.size
+      end
+      alias :length :size 
+
+      def each
+        @set.each { |entry| yield entry }
       end
     end
   end
--- test/compiler/classpath_spec.rb
+++ test/compiler/classpath_spec.rb
@@ -3,20 +3,64 @@
 require_relative "../helper"
 
 describe Doubleshot::Compiler::Classpath do
+  before do
+    @classpath = Doubleshot::Compiler::Classpath.new
+  end
+
   describe "add" do
     it "must return self" do
       Helper::tmp do |tmp|
-        classpath = Doubleshot::Compiler::Classpath.new
-        classpath.add(tmp.to_s).must_be_same_as classpath
+        @classpath.add(tmp.to_s).must_be_same_as @classpath
       end
     end
 
     it "must raise an exception if the file does not exist" do
       Helper::tmp do |tmp|
         -> do
-          Doubleshot::Compiler::Classpath.new.add(tmp + "asdf")
+          @classpath.add(tmp + "asdf")
         end.must_raise(Errno::ENOENT)
       end
+    end
+  end
+
+  it "must return Pathnames" do
+    Helper::tmp do |tmp|      
+      @classpath.add tmp
+
+      @classpath.entries.wont_be_empty
+      @classpath.each do |path|
+        path.must_be_kind_of Pathname
+      end
+    end
+  end
+
+  it "must return unique paths" do
+    Helper::tmp do |tmp|
+      @classpath.add(tmp.to_s)
+      @classpath.add(tmp.to_s)
+
+      @classpath.size.must_equal 1
+    end
+  end
+
+  it "must return only directories" do
+    Helper::tmp do |tmp|
+      dir1 = tmp + "dir1"
+      dir2 = tmp + "dir2"
+
+      dir1.mkdir
+      dir2.mkdir
+
+      jar1 = dir1.touch "some.jar"
+      jar2 = dir2.touch "other.jar"
+
+      @classpath.add dir1
+      @classpath.add jar1
+      @classpath.add jar2
+
+      @classpath.size.must_equal 2
+      @classpath.must_include(tmp + "dir1")
+      @classpath.must_include(tmp + "dir2")
     end
   end
 end
